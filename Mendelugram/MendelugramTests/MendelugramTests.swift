98660ce1fed8ae20a6ff87a958991d4f342b8a33
Tomas Cejka <tomas.cejka@strv.com>
2020-04-23 08:21:40 +0200

    [add] testing targets

---
+++ Mendelugram/MendelugramTests/MendelugramTests.swift
@@ -0,0 +1,33 @@
+//
+//  MendelugramTests.swift
+//  MendelugramTests
+//
+//  Created by Tomas Cejka on 4/22/20.
+//  Copyright © 2020 ČVUT. All rights reserved.
+//
+
+import XCTest
+
+class MendelugramTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
